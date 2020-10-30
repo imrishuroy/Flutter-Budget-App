322f24b037ba475e34e7dff0f2c497c02fc532cb
Rishu Roy <rishukumar.prince@gmail.com>
2020-10-30 23:05:32 +0530

    Added SliverAppBar to HomePage

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -4,7 +4,40 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            forceElevated: true,
+            floating: true,
+            // pinned: true,
+            expandedHeight: 100.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('Simple Budget'),
+            ),
+            leading: IconButton(
+              icon: Icon(Icons.settings),
+              iconSize: 30.0,
+              onPressed: () {},
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.add),
+                iconSize: 30.0,
+                onPressed: () {},
+              ),
+            ],
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate((BuildContext context, index) {
+              return Container(
+                margin: EdgeInsets.all(10),
+                height: 100,
+                color: Colors.yellow,
+              );
+            }, childCount: 10),
+          )
+        ],
+      ),
     );
   }
 }
